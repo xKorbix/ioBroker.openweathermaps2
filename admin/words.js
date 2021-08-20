accee38cc0f7e1b9a341acef323b0474fd48e6c0
Korbinian <korbinian@schuh-zechmeister.de>
2021-08-20 08:21:06 +0200

    first commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+'use strict';
+
+systemDictionary = {
+    'openweathermaps2 adapter settings': {
+        'en': 'Adapter settings for openweathermaps2',
+        'de': 'Adaptereinstellungen für openweathermaps2',
+        'ru': 'Настройки адаптера для openweathermaps2',
+        'pt': 'Configurações do adaptador para openweathermaps2',
+        'nl': 'Adapterinstellingen voor openweathermaps2',
+        'fr': "Paramètres d'adaptateur pour openweathermaps2",
+        'it': "Impostazioni dell'adattatore per openweathermaps2",
+        'es': 'Ajustes del adaptador para openweathermaps2',
+        'pl': 'Ustawienia adaptera dla openweathermaps2',
+        'zh-cn': 'openweathermaps2的适配器设置'
+    },
+    'option1': {
+        'en': 'option1',
+        'de': 'Option 1',
+        'ru': 'Опция 1',
+        'pt': 'Opção 1',
+        'nl': 'Optie 1',
+        'fr': 'Option 1',
+        'it': 'opzione 1',
+        'es': 'Opción 1',
+        'pl': 'opcja 1',
+        'zh-cn': '选项 1'
+    },
+    'option2': {
+        'en': 'option2',
+        'de': 'Option 2',
+        'ru': 'option2',
+        'pt': 'opção 2',
+        'nl': 'Optie 2',
+        'fr': 'Option 2',
+        'it': 'opzione 2',
+        'es': 'opcion 2',
+        'pl': 'Opcja 2',
+        'zh-cn': '选项2'
+    }
+};
